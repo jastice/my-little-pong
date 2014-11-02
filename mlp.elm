9095ae6de0abd67b6a97796e06ca343091c73457
Justin Kaeser <github@accounts.justinkaeser.com>
2014-11-02 15:43:35 +0100

    fresh balls!

--- mlp.elm
+++ mlp.elm
@@ -1,20 +1,16 @@
 import Keyboard
 import Debug
 
-type Vec2 = (Float,Float)
-type Ball = { pos: Vec2, direction: Float }
+type Ball = { pos: (Float,Float), direction: Float }
 
 dim = { x=1000, y=800 }
 
-bounds = {
-  xh = (toFloat dim.x) / 2,
-  yh = (toFloat dim.y) / 2
-  }
+bounds = { xh = (toFloat dim.x) / 2, yh = (toFloat dim.y) / 2 }
 
 ballsize = 30
 
-luna y = fittedImage 100 100 "img/luna_400.png" |> toForm |> move ( -bounds.xh + 50, y)
-celestia y = fittedImage 100 100 "img/celestia_400.png" |> toForm |> move ( bounds.xh - 50, y)
+luna y = image 100 100 "img/luna_400.png" |> toForm |> move ( -bounds.xh + 50, y)
+celestia y = image 100 100 "img/celestia_400.png" |> toForm |> move ( bounds.xh - 50, y)
 ball pos = circle ballsize |> gradient (radial (10,10) 0 (0,20) 50 [(0,red),(0.8,blue)]) |> move pos
 
 scene lunaY celestiaY ballXY = collage dim.x dim.y [luna lunaY, celestia celestiaY, ball ballXY]
@@ -45,25 +41,30 @@ checkBounds ball =
 checkPonies: (Float,Float) -> Ball -> Ball
 checkPonies (posLuna,posCelestia) ball = 
   let lunaBound = -bounds.xh + 100
-      lunaTop = posLuna + 50
-      lunaBottom = posLuna - 50
+      lunaTopBottom = (posLuna + 50, posLuna - 50)
       celestiaBound = bounds.xh - 100
-      celestiaTop = posCelestia + 50
-      celestiaBottom = posCelestia - 50
+      celestiaTopBottom = (posCelestia + 50, posCelestia - 50)
       (ballX, ballY) = ball.pos
-      isReflected yTop yBottom = ballY <= yTop && ballY >= yBottom
-  in if ballX <= lunaBound && isReflected lunaTop lunaBottom ||
-        ballX >= celestiaBound && isReflected celestiaTop celestiaBottom
+      isReflected (yTop,yBottom) = ballY <= yTop && ballY >= yBottom
+  in if ballX <= lunaBound && isReflected lunaTopBottom ||
+        ballX >= celestiaBound && isReflected celestiaTopBottom
      then {ball | direction <- pi-ball.direction}
+     else ball
+
+checkReset: Ball -> Ball
+checkReset ball = 
+  let (x,y) = ball.pos
+  in if x < -bounds.xh || x > bounds.xh
+     then { pos = (0,0), direction = -(pi-ball.direction) }
      else ball
 
 updateBall: (Float,Float) -> Ball -> Ball
 updateBall ponies ball = 
   let ball1 = checkBounds ball
-      {pos,direction} = checkPonies ponies ball1
+      ball2 = checkPonies ponies ball1
+      {pos,direction} = checkReset ball2
       pos1 = times (cos direction, sin direction) 5 |> plus pos
   in { pos = pos1, direction = direction }
-
 
 -- signals
 
